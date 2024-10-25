4e4161f5f0787c3725f9d1da09e4a1d588a97365
Nikhil-9876 <nikhilsolanki9876@gmail.com>
2024-10-25 13:01:02 +0530

    admin completed by princy

--- Assignment.java
+++ Assignment.java
@@ -1,3 +1,21 @@
+
+// Movie class inheriting from Show
+// class Movie extends Show {
+//     public Movie(String title, int duration, int availableSeats) {
+//         super(title, duration, availableSeats);
+//     }
+
+//     @Override
+//     public void showDetails() {
+//         System.out.println("Movie: "+gettitle);
+//         System.out.println("Time: "+gettime());
+//         System.out.println("Duration: "+duration+" minutes");
+//         System.out.println("Available seats: "+availableseats);
+//         System.out.println("Seat matrix: \n");
+//         printseatmatrix();
+//     }
+// }
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -43,42 +61,42 @@ class show{
     int duration;
     int availableseats;
     String time;
+    int Screen;
     int seatmatrix[][] = new int[7][7];
 
 
-    show(String title,int duration,int availableseats,String time,int occupiedseats){
+    show(String title,int duration,int availableseats,String time,int occupiedseats,int Screen,Scanner sc){
         this.title=title;
         this.duration=duration;
         this.time=time;
         this.availableseats=availableseats;
+        this.Screen = Screen;
         if(occupiedseats!=0)System.out.println("Enter coordinates of reserved seats:");
-        Scanner sc=new Scanner(System.in);
         if(occupiedseats!=0){
             for(int i=0;i<occupiedseats;i++){
                 System.out.println("enter row no. and column no. [7*7]:");
                 int r=sc.nextInt();
                 int c=sc.nextInt();
                 sc.nextLine();
-                seatmatrix[r-1][c-1]=1;
+                this.seatmatrix[r-1][c-1]=1;
             }
         }
-        sc.close();
     } 
 
-    public String gettitle(){
-        return this.title;
-    }
-    public String gettime(){
-        return this.time;
-    }
-    int getavailableseats(){
-        return this.availableseats;
-    }
+    // public String gettitle(){
+    //     return this.title;
+    // }
+    // public String gettime(){
+    //     return this.time;
+    // }
+    // int getavailableseats(){
+    //     return this.availableseats;
+    // }
 
 
     void printseatmatrix(){
         System.out.println("1=Reserved seat and 0=available seat");
-        System.out.println( " ___________ \n");
+        System.out.println( " ___________ \n"); // seat matrix screen
         for(int i=0;i<7;i++){
             for(int j=0;j<7;j++){
                 System.out.print(seatmatrix[i][j]+" ");
@@ -92,6 +110,7 @@ class show{
         System.out.println("Movie: "+title);
         System.out.println("Time: "+time);
         System.out.println("Duration: "+duration+" minutes");
+        System.out.println("Screen : "+Screen);
         System.out.println("Available seats: "+availableseats);
         System.out.println("Seat matrix: \n");
         printseatmatrix();
@@ -99,31 +118,13 @@ class show{
 
 }
 
-// Movie class inheriting from Show
-// class Movie extends Show {
-//     public Movie(String title, int duration, int availableSeats) {
-//         super(title, duration, availableSeats);
-//     }
-
-//     @Override
-//     public void showDetails() {
-//         System.out.println("Movie: "+gettitle);
-//         System.out.println("Time: "+gettime());
-//         System.out.println("Duration: "+duration+" minutes");
-//         System.out.println("Available seats: "+availableseats);
-//         System.out.println("Seat matrix: \n");
-//         printseatmatrix();
-//     }
-// }
-
-class cinema{
-    String name;
+class Theatre{
     ArrayList<show> shows;
 
-    cinema(String name){
-        this.name=name;
+    Theatre(){
         this.shows=new ArrayList<>();
     }
+
     void addshow(show s){
         shows.add(s);
     }
@@ -146,6 +147,17 @@ class cinema{
         if(c==0) System.out.println("No shows for the movie you a looking for");
     }
 
+    void printshowsbyScreen(int Screen){
+        int c=0;
+        for(int i=0;i<shows.size();i++){
+            if(shows.get(i).Screen==Screen){
+                shows.get(i).displayshow();
+                c++;
+            }
+        }
+        if(c==0) System.out.println("No shows for the movie you a looking for");
+    }
+
     void printshowsbytime(String time){
         int c=0;
         for(int i=0;i<shows.size();i++){
@@ -157,42 +169,33 @@ class cinema{
         if(c==0) System.out.println("No shows for the timespan you a looking for");
     }
 
+    void change(String name, String time, int screen){
+        Scanner sc = new Scanner(System.in);
+        int i=0;
+        for(;i<shows.size();i++){
+            if(shows.get(i).title.equals(name) && shows.get(i).time.equals(time) && shows.get(i).Screen==screen){
+                break;
+            }
+        }
+        System.out.println("Enter new time of the movie : ");
+        shows.get(i).time = sc.nextLine();
+        System.out.println("Enter new Screen of the movie : ");
+        shows.get(i).Screen = sc.nextInt();
+    }
+
 }
 
 
 public class Assignment {
     public static void main(String args[]){
-        // ArrayList <show> shows=new ArrayList<>();//to be declared in cinema
-        cinema cinema = new cinema("Cineplex");
+        Theatre cinema = new Theatre();
         Scanner sc = new Scanner(System.in);
         Boolean cond = true;
         String identity;
-        int f=0,f1=0;
         while(cond){
-            if(f!=0) sc.nextLine();
-            f++;
             Boolean condUser=true, condAdmin=true;
             System.out.println("Admin or User or Exit ? ");
-            identity = sc.nextLine();
-            //an input buffer error is here 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            // System.out.println(identity);
+            identity = sc.next();
             if(identity.equals("admin")){ // if person is admin
                 // System.out.println("Enter password to login : ");
                 // check admin = new check();
@@ -216,14 +219,13 @@ public class Assignment {
                 // }
                 
                 while(condAdmin){
-                    if(f1!=0) sc.nextLine();
-                    f1++;
                     String choice;
-                    System.out.println("Add movie, delete movie, display movie, exit ? ");//choose the operation to do
-                    choice=sc.nextLine();
+                    System.out.println("Add movie, delete movie, display movie, Change movie, exit ? ");//choose the operation to do
+                    choice=sc.next();
                     if(choice.equals("add")){
                         String title,time;
-                        int duration,availableSeats,occupiedseats;
+                        int duration,availableSeats,occupiedseats, Screen;
+                        sc.nextLine();
                         System.out.println("Enter title of the movie : ");
                         title = sc.nextLine();
                         System.out.println("Enter time of the movie : ");
@@ -232,44 +234,52 @@ public class Assignment {
                         duration=sc.nextInt();
                         System.out.println("Enter availableseats of the movie : ");
                         availableSeats=sc.nextInt();
+                        System.out.println("Enter Screen of the movie : ");
+                        Screen=sc.nextInt();
                         occupiedseats=49-availableSeats;
-                        show s = new show(title,duration,availableSeats,time,occupiedseats);
-                        // s.displayshow();
+                        show s = new show(title,duration,availableSeats,time,occupiedseats,Screen,sc);
                         cinema.addshow(s);
-                        sc.nextLine();
                     }else if(choice.equals("delete")){
                         String title,time;
+                        sc.nextLine();
                         System.out.println("Enter title of the movie : ");//method to delete movie in cinema
                         title = sc.nextLine();
                         System.out.println("Enter time of the movie : ");
                         time = sc.nextLine();
                         for(int i=0;i<cinema.shows.size();i++){
-                            String  showtitle = cinema.shows.get(i).gettitle();
-                            String  showtime = cinema.shows.get(i).gettime();
-                            if(showtime==time && showtitle==title){
+                            if(cinema.shows.get(i).time.equals(time) && cinema.shows.get(i).title.equals(title)){
                                 cinema.shows.remove(i);
                             }
                         }
+                        // count of movire decrease krvanu baki
                     }else if(choice.equals("display")){
                         for(int i=0;i<cinema.shows.size();i++){
                             System.out.println("----------------------");
                             show s=cinema.shows.get(i);// method to be declared inside cinema class
                             s.displayshow();
                         }
+                    }else if(choice.equals("change")){
+                        System.out.println("Enter movie name : ");
+                        String chngName = sc.nextLine();
+                        System.out.println("Enter movie time : ");
+                        String chngTime = sc.nextLine();
+                        System.out.println("Enter movie name : ");
+                        int chngScreen = sc.nextInt();
+                        cinema.change(chngName, chngTime, chngScreen);
                     }else{
                         condAdmin=false;
                     }
                 }
             }else if(identity.equals("user")){ // if person is user
                 System.out.println("user");
-                // while(condUser){
-                // }
+                while(condUser){
+                    
+                }
             }else{
                 cond=false;
             }
         }
         
         sc.close();
-        //check
     }
 }
